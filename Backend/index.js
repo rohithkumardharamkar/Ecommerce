ba6ab0ec1289805c1c74ba35aab7ce23671d7ea5
Dharamkar Rohith Kumar <159691874+rohithkumardharamkar@users.noreply.github.com>
2024-11-29 08:05:29 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,7 @@ const proute = require("./route/product.route");
 const userRoute = require("./route/user.route");
 let app=express()
 mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce").then((res)=>{console.log("database connected");}).catch((err)=>{console.log(err);})
-app.use(cors({
-origin: "https://ecommerce-hpwo.vercel.app/",
-credentials: true
-}))
-
+app.use(cors({origin: "https://ecommerce-hpwo.vercel.app/",}))
 let port=process.env.PORT || 5000
 app.use(express.json())
 app.get("/",(req,res)=>
