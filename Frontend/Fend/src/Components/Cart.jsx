7f5ed0ddd5466263a5b0ffe9c25a188182576b2c
rohithkumardharamkar <rohithkumardharamkar@gmail.com>
2024-12-20 12:37:11 +0530

    Switch to bcryptjs to avoid native binding issues

--- Frontend/Fend/src/Components/Cart.jsx
+++ Frontend/Fend/src/Components/Cart.jsx
@@ -6,96 +6,83 @@ import { url } from '../../url'
 
 
 const Cart = () => {
-    let obj=useContext(Ct)
-    let [cart,setCart]=useState([])
-    let navigate=useNavigate()
-    let fetch=()=>
-    {
+    let obj = useContext(Ct)
+    let [cart, setCart] = useState([])
+    let navigate = useNavigate()
+
+    let fetch = () => {
         console.log("kkkkkkkk");
         console.log(obj.cont.token);
-    
-        axios.get(`${url}/cart/getcart/${obj.cont._id}`,{"headers":{"Authorization":obj.cont.token}}).then((res)=>
-        {
-            setCart(res.data)
-          
-        }).catch((err)=>
-    {
-        console.log(err);
-    })
 
+        axios.get(`${url}/cart/getcart/${obj.cont._id}`, { "headers": { "Authorization": obj.cont.token } }).then((res) => {
+            setCart(res.data)
+        }).catch((err) => {
+            console.log(err);
+        })
     }
-    useEffect(()=>{
-        if(obj.cont.token=="")
-        {
+
+    useEffect(() => {
+        if (obj.cont.token === "") {
             navigate("/login")
-        }
-        else
-        {
+        } else {
             fetch()
         }
+    }, [])
 
-    },[])
-  
- 
-    let inc=(_id)=>{
+    let inc = (_id) => {
         console.log(_id);
-        axios.put(`${url}/cart/inc`,{"_id":_id},{"headers":{"Authorization":obj.cont.token}}).then((res)=>{
+        axios.put(`${url}/cart/inc`, { "_id": _id }, { "headers": { "Authorization": obj.cont.token } }).then((res) => {
             console.log(res);
-          fetch()
+            fetch()
         })
-      }
-      let dec=(_id)=>{
-        console.log(_id);
-        axios.put(`${url}/cart/dec`,{"_id":_id},{"headers":{"Authorization":obj.cont.token}}).then((res)=>{
-            console.log(res);
-          fetch()
-        })
-      }
-let clear=()=>
-{
-    axios.delete(`${url}/cart/clear/${obj.cont._id}`,{"headers":{"Authorization":obj.cont.token}}).then(()=>
-{
-    fetch()
-})
-}       
-let del=(_id)=>
-{
-    console.log(_id);
-    axios.delete(`${url}/cart/delete/${_id}`,{"headers":{"Authorization":obj.cont.token}}).then(()=>
-{
-    fetch()
-})
-}
+    }
 
-  
-  return (
-    <div>
-        {cart.length==0 && <div className='empty'>Empty Cart</div>}
-        {cart.length!=0 && <div className='pcon'>
-            {
-                cart.map((item)=>
-            {
-                return(<div className='product'>
-<<<<<<< HEAD
-                <div><img src={`${url}/images/${item.productimg}`}alt="Product Image" /></div>
-=======
-                <div><img src={`${url}/images/${item.productimg}`}alt="Product Image" /></div>
->>>>>>> 6b1d35bb2e9ad07929bedf85d0c3c7cc92d49e30
-        <h3>{item.name}</h3>
-        <p>Category :{item.category}</p>
-        <p>Price :{item.price}</p>
-       <button onClick={()=>dec(item._id)}>-</button>
-       <p>{item.qty}</p>
-       <button onClick={()=>inc(item._id)}>+</button>
-       <button onClick={()=>del(item._id)}>Remove Product</button>
-                </div>)
-            })
-            }
+    let dec = (_id) => {
+        console.log(_id);
+        axios.put(`${url}/cart/dec`, { "_id": _id }, { "headers": { "Authorization": obj.cont.token } }).then((res) => {
+            console.log(res);
+            fetch()
+        })
+    }
+
+    let clear = () => {
+        axios.delete(`${url}/cart/clear/${obj.cont._id}`, { "headers": { "Authorization": obj.cont.token } }).then(() => {
+            fetch()
+        })
+    }
+
+    let del = (_id) => {
+        console.log(_id);
+        axios.delete(`${url}/cart/delete/${_id}`, { "headers": { "Authorization": obj.cont.token } }).then(() => {
+            fetch()
+        })
+    }
+
+    return (
+        <div>
+            {cart.length === 0 && <div className='empty'>Empty Cart</div>}
+            {cart.length !== 0 && <div className='pcon'>
+                {
+                    cart.map((item) => {
+                        return (
+                            <div className='product' key={item._id}>
+                                <div><img src={`${url}/images/${item.productimg}`} alt="Product Image" /></div>
+                                <h3>{item.name}</h3>
+                                <p>Category : {item.category}</p>
+                                <p>Price : {item.price}</p>
+                                <button onClick={() => dec(item._id)}>-</button>
+                                <p>{item.qty}</p>
+                                <button onClick={() => inc(item._id)}>+</button>
+                                <button onClick={() => del(item._id)}>Remove Product</button>
+                            </div>
+                        )
+                    })
+                }
             </div>}
 
-    <button className='cartdel' onClick={clear}>Delete Cart</button>
-    </div>
-  )
+            <button className='cartdel' onClick={clear}>Delete Cart</button>
+        </div>
+    )
 }
 
 export default Cart
--- Frontend/Fend/src/Components/Login.jsx
+++ Frontend/Fend/src/Components/Login.jsx
@@ -1,65 +1,52 @@
 import React, { useContext, useState } from 'react'
-import {useNavigate }from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './Nav.css'
 import axios from 'axios'
 import Ct from './Ct'
 import { url } from '../../url'
 
 const Login = () => {
-    let [data,setData]=useState({"_id":"","pwd":""})
-    let [err,setErr]=useState("")
-    let obj=useContext(Ct)
-    let navigate=useNavigate()
-    
-    function fun(e)
-    {
-        setData({...data,[e.target.name]:e.target.value})
+    let [data, setData] = useState({ "_id": "", "pwd": "" })
+    let [err, setErr] = useState("")
+    let obj = useContext(Ct)
+    let navigate = useNavigate()
+
+    function fun(e) {
+        setData({ ...data, [e.target.name]: e.target.value })
     }
-    
-   console.log(err);
-    function login()
-    {
-        if(data.pwd=="" && data._id=="")
-        {
+
+    console.log(err);
+
+    function login() {
+        if (data.pwd === "" && data._id === "") {
             setErr("Fill all details")
         }
-       console.log(data);
-    console.log(err);
-<<<<<<< HEAD
-    https://ecommerce-yrgm.vercel.app/
-=======
->>>>>>> 6b1d35bb2e9ad07929bedf85d0c3c7cc92d49e30
-        axios.post(`${url}/user/login`,data).then((res)=> {   
-            if(res.data.token)
-            {
+        console.log(data);
+        console.log(err);
+
+        axios.post(`${url}/user/login`, data).then((res) => {
+            if (res.data.token) {
                 obj.updcont(res.data)
                 navigate("/")
-                
-
-            }
-            else
-            {
+            } else {
                 setErr(res.data.msg)
             }
-         })
-        
-
-
+        })
     }
+
     console.log(data);
-  return (
-    <div>
-        <div className="login">
-            <h1>Login</h1>
-            <input type="text" placeholder='Enter username or email-id' onChange={fun} name='_id' value={data._id}/>
-            <input type="password" placeholder='Enter Password' onChange={fun} name='pwd' value={data.pwd}/>
-         
-            <button onClick={login}>LOGIN</button>
-            <h3>{err}</h3>
+
+    return (
+        <div>
+            <div className="login">
+                <h1>Login</h1>
+                <input type="text" placeholder='Enter username or email-id' onChange={fun} name='_id' value={data._id} />
+                <input type="password" placeholder='Enter Password' onChange={fun} name='pwd' value={data.pwd} />
+                <button onClick={login}>LOGIN</button>
+                <h3>{err}</h3>
+            </div>
         </div>
-     
-    </div>
-  )
+    )
 }
 
 export default Login
