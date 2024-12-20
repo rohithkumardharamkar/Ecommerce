6b6cc4785798bbcadd8e43d483eb03af7d5e10d2
rohithkumardharamkar <rohithkumardharamkar@gmail.com>
2024-12-20 11:42:18 +0530

    Resolved conflicts and added updates to Backend and Frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-let express=require("express");
-let cors=require("cors");
-let mongoose=require("mongoose");
-var bodyParser = require('body-parser');
-const cartRouter = require("./route/cart.route");
-const proute = require("./route/product.route");
-const userRoute = require("./route/user.route");
-let app=express()
-require('dotenv').config()
-
-// mongodb://127.0.0.1:27017/Ecommerce
-mongoose.connect(process.env.mongo_url).then((res)=>{console.log("database connected");}).catch((err)=>{console.log(err);})
-app.use(cors());
-let port=process.env.PORT || 5000
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/cart",cartRouter)
-app.use("/products",proute)
-app.use("/user",userRoute)
-app.use("/images",express.static("./images"))
-app.listen(port)
-=======
 let express = require("express");
 let cors = require("cors");
 let mongoose = require("mongoose");
@@ -29,20 +6,17 @@ require("dotenv").config();
 const cartRouter = require("./route/cart.route");
 const proute = require("./route/product.route");
 const userRoute = require("./route/user.route");
+
 let app = express();
-let port = process.env.PORT || 3002;  
+let port = process.env.PORT || 5000;
 
+mongoose.connect(process.env.mongo_url).then((res) => {
+  console.log("Database connected");
+}).catch((err) => {
+  console.log(err);
+});
 
-mongoose.connect("mongodb+srv://rohithkumardharamkar:Chintu@431431@cluster0.ji041.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=Cluster0").then((res)=>{console.log("database connected");}).catch((err)=>{console.log(err);})
-
-app.use(
-  cors({
-    origin: "https://ecommerce-gws5.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -65,4 +39,3 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
->>>>>>> 6b1d35bb2e9ad07929bedf85d0c3c7cc92d49e30
